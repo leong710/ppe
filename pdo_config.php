c2aac19b96f46eb8977cd0eb5b28065cd62c94fc
leong710 <leong710@gmail.com>
2024-11-27 17:05:44 +0800

    移機更新

--- pdo_config.php
+++ pdo_config.php
@@ -6,7 +6,7 @@
         'db_user'      => 'tnesh',
         'db_pass'      => '&9t040500',
         
-        'hrdb_host'    => '10.53.90.163,1433',
+        'hrdb_host'    => '10.53.203.100,1433',
         'hrdb_name'    => 'hrdb',
         'hrdb_charset' => 'utf8mb4',
         'hrdb_user'    => 'tnesh',
